2d9cb6c8232aee074e8388acbd54fe6e1770cadf
Jason20668 <526JBach@frhsd.com>
2025-02-26 09:27:09 -0500

    Everything except clear all tasks button

--- script.js
+++ script.js
@@ -2,17 +2,52 @@
 let tasks = []
 
 //Add event listener to add tasks
-document.getElementById('addTaskButton').addEventListener('click', function(){
-//Saving text value from input box as a variable "taskInput"
+document.getElementById('addTaskButton').addEventListener('click', function () {
+    //Saving text value from input box as a variable "taskInput"
     let taskInput = document.getElementById('taskInput').value
-//If statement checking the truthy falsy of the variable
-    if (taskInput){
-//If its truthy it pushes the value to the last issue of the array
-    tasks.push(taskInput)
-//Makes it so the value for Task Input is nothing after the button is clicked
-    document.getElementById('taskInput').value = ""
-//function that isn't set up yet
-    displayTasks()
+    //If statement checking the truthy falsy of the variable
+    if (taskInput) {
+        //If truthy  pushes the value to the last issue of the array
+        tasks.push(taskInput)
+        //Value for Task Input is wiped after the button is clicked
+        document.getElementById('taskInput').value = ""
+        //function that isn't set up yet
+        displayTasks()
+
     }
-})
+}
+)
+
+//Function to display tasks inputted
+function displayTasks() {
+    //Grabbing the ul in the HTML
+    let taskList = document.getElementById('taskList')
+    //Clear all tasks when 1 is added
+    taskList.innerHTML = ''
+    //Loop through each task in the array and create a list item
+    tasks.forEach((task, index) => {
+        //Create a new <li> for each task
+        let li = document.createElement('li')
+        //Give that li element some Bootstrap
+        li.classList.add(
+            'list-group-item',
+            'd-flex',
+            'justify-content-between',
+            'align-items-center'
+        )
+
+        //Set inner HTML of <li> tasktext with button and 
+        li.innerHTML = `${task} <button class='btn btn-success btn-sm' onclick='removeTask(${index})'>√</button>`
+        //Append new task to taskList
+        taskList.appendChild(li)
+    })
+}
+
+//Function that removes a task when checkmark is clicked
+function removeTask(index){
+//Remove task from array
+    tasks.splice(index, 1)
+//Call the function to update the task list display
+displayTasks()
+}
 
